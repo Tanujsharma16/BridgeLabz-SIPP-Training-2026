62dd61a0952550888ad6d067253018cb7e9c4d47
Tanuj Sharma <tanuj@example.com>
2026-06-09 22:35:44 +0530

    [Tanuj Sharma] Added: Level-1

---
+++ core-java-practice/gcr-codebase/control-flow/Level-1/CountdownFor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CountdownFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = sc.nextInt();
+
+        for (; counter >= 1; counter--) {
+            System.out.println(counter);
+        }
+    }
+}
